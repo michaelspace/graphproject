33d44b71b8ccaa0c648b260d82df2cbbe5f1f3e2
MichalP <pazdziorekmichal@gmail.com>
2018-06-02 12:01:28 +0200

    Dodanie modelu wierzchołków + clean code

--- graphproject/MainWindow.xaml.cs
+++ graphproject/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using graphproject.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,21 +28,16 @@ namespace graphproject
         public bool pierwszaKrawedz;
         public double x1;
         public double y1;
+        public List<Wierzcholek> wierzcholki;
+        public int id;
 
-        int[,] graph_matrix = { { 0, 1, 2, 5, 3},
-                                    {1, 0, 0, 1, 0},
-                                    {2, 0, 0, 3, 0},
-                                    {5, 1, 3, 0, 0},
-                                    {3, 0, 0, 0, 0} };
 
         public MainWindow()
         {
             InitializeComponent();
             wierzcholek = false;
             pierwszaKrawedz = true;
-            
-
-            Floyd_Warshall(graph_matrix);
+            id = 1;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -72,6 +68,9 @@ namespace graphproject
                 canvas.Children.Add(circle);
                 canvas.Children.Add(label);
 
+
+                Wierzcholek w = new Wierzcholek(id, NazwaWierzcholka.Text, Mouse.GetPosition(this).X - 45, Mouse.GetPosition(this).Y - 45);
+                wierzcholki.Add(w);
                 this.Cursor = Cursors.AppStarting;
                 wierzcholek = false;
             }
@@ -103,104 +102,6 @@ namespace graphproject
                     
         }
 
-        const int inf = int.MaxValue / 2 - 1; //infinity div 2
-
-        static void Floyd_Warshall(int[,] graph_matrix)
-        {
-            // "distance" array contains lenght of the shortest path between every pair of vertices
-            // "prev" array contains vertices (separated by dots) which are in particular path
-
-
-            int n = graph_matrix.GetLength(0);
-            n++; //algorithm works on matrix which size is n+1
-
-            int[,] matrix = new int[n, n];
-
-            for (int i = 0; i < n; i++) matrix[i, 0] = 0;
-            for (int j = 0; j < n; j++) matrix[0, j] = 0;
-
-            for (int i = 1; i < n; i++) //extending matrix to n+1 size
-            {
-                for (int j = 1; j < n; j++)
-                {
-                    matrix[i, j] = graph_matrix[i - 1, j - 1];
-                }
-            }
-
-            int[,,] dist = new int[n, n, n]; //shortest distance between every pair of vertices
-            string[,] prev = new string[n, n]; //course of the shortest path between vertices
-
-            for (int i = 1; i < n; i++) //initial part of the algorithm
-            {
-                for (int j = 1; j < n; j++)
-                {
-                    if (matrix[i, j] != 0)
-                    {
-                        dist[i, j, 0] = matrix[i, j];
-                        prev[i, j] = Convert.ToString(i)+'.'; //vertices in path are separated by dots
-                    }
-
-                    else
-                    {
-                        dist[i, j, 0] = inf;
-                        prev[i, j] = Convert.ToString(i)+'.';
-                    }
-                }
-            }
-
-            for (int k = 1; k < n; k++) //main part of the algorithm
-            {
-                for (int i = 1; i < n; i++)
-                {
-                    for (int j = 1; j < n; j++)
-                    {
-                        if (dist[i, k, k - 1] + dist[k, j, k - 1] < dist[i, j, k - 1])
-                        {
-                            dist[i, j, k] = dist[i, k, k - 1] + dist[k, j, k - 1];
-                            if (i == j) prev[i, j] = "null";
-                            else prev[i, j] = prev[i, j] + Convert.ToString(k)+'.';
-                        }
-                        else dist[i, j, k] = dist[i, j, k - 1];
-                    }
-                }
-            }
-
-            int[,] distance = new int[n, n];
-
-            for (int i = 0; i < n; i++) //converting 3-dimension "dist" matrix to 2-dimension "distance" matrix with constant k = size of matrix
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    if (i == j) distance[i, j] = 0;
-                    else distance[i, j] = dist[i, j, n - 1]; //n-1
-                }
-            }
-
-            for (int i = 1; i < n; i++) //adding last vertice to path
-            {
-                for (int j = 1; j < n; j++)
-                {
-                    if (prev[i, j] != "null") prev[i, j] = prev[i, j] + Convert.ToString(j);
-                }
-            }
-
-            Console.WriteLine("-----DISTANCE-----");
-            Display<int>(distance);
-            Console.WriteLine("-----PATH-----");
-            Display<string>(prev);
-        }
-
-        static void Display<T>(T[,] matrix)
-        {
-            for (int i = 1; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 1; j < matrix.GetLength(0); j++)
-                {
-                    Console.Write(matrix[i, j] + ", ");
-                }
-                Console.WriteLine();
-            }
-        }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
---
+++ graphproject/Model/Wierzcholki.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graphproject.Model
+{
+    public class Wierzcholek
+    {
+        public int id;
+        public string nazwa;
+        public double wspolrzednaX;
+        public double wspolrzednaY;
+
+        public Wierzcholek(int id, string nazwa, double wspolrzednaX, double wspolrzednaY)
+        {
+            this.id = id;
+            this.nazwa = nazwa;
+            this.wspolrzednaX = wspolrzednaX;
+            this.wspolrzednaY = wspolrzednaY;
+        }
+    }
+}
